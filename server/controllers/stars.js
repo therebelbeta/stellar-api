4f1c2a6bdac15192dbdbd21a124c181d63374e91
Trent Oswald <trentoswald@therebelrobot.com>
2014-12-12 15:38:13 -0700

    added secrets template

--- server/controllers/stars.js
+++ server/controllers/stars.js
@@ -32,7 +32,80 @@ module.exports = {
     });
   },
   syncStars: function(req, res, next) {
+    var userid = req.user.github;
+    var token = req.user.tokens[0].accessToken;
 
+    Repo.find({
+      id: req.user.github
+    }, function(err, data) {
+      if (err) {
+        res.json(err);
+      }
+      else {
+        data = data[0];
+        var thisd = new Date();
+        thisd = thisd.getTime();
+        if (data.lastSynced > thisd + 43200000 || !data.lastSynced || req.query
+          .force) {
+          var page = 1;
+          var funcArray = [];
+          var totalResults = [];
+
+          function createRequestFunction(pageIndex) { 
+            return function(callback) {
+              var options = {
+                hostname: 'https://api.github.com',
+                path: '/user/' + userid + '/starred?access_token=' +
+                  token + '&per_page=100&page=' + pageIndex,
+                headers: {
+                  'User-Agent': 'Constella',
+                  'Accept': 'application/json'
+                }
+              };
+              unirest.get(options.hostname + options.path)
+                .headers(options.headers)
+                .end(function(response) {
+                  totalResults = _.union(totalResults, response.body);
+                  callback(false, data)
+                });
+            }
+          }
+          for (var i = 1; i <= 10; i++) { 
+            funcArray.push(createRequestFunction(i))
+          }
+          async.parallel(funcArray, function(err, results) {
+            if (err) {
+              res.json({
+                'error': results
+              })
+            }
+            var allStars = [];
+            for (var i = results.length - 1; i >= 0; i--) {
+              if (results[i][0]) {
+                if (results[i][0].length === 0) {
+                  results.splice(i, 1);
+                }
+                else {
+                  var allStars = results[i].concat(allStars);
+                }
+              }
+            }
+            data.repos.remote = allStars;
+            var d = new Date()
+            data.lastSynced = d.getTime();
+            data.save(function(err) {
+              if (err) return res.json({
+                'error': err
+              });
+              res.json('sync success');
+            });
+          });
+        }
+        else {
+          res.json('already synced');
+        }
+      }
+    });
   },
   getStar: function(req, res, next) {
 
--- server/index.js
+++ server/index.js
@@ -5,10 +5,18 @@ var server = restify.createServer();
 var mongoose = require('./model/mongoose').init();
 
 var passportHelper = require('./helpers/passport');
+var secrets = require('./config/secrets');
 var routes = require('./routes');
 
 // set up server
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.authorizationParser());
+server.use(restify.dateParser());
 server.use(restify.queryParser());
+server.use(restify.jsonp());
+server.use(restify.gzipResponse());
+server.use(restify.bodyParser());
+
 server.use(passport.initialize());
 
 // initiate routes and passport
@@ -16,6 +24,6 @@ routes.init(server);
 passportHelper.gh_passport();
 
 // Start the app by listening on <port>
-var port = process.env.PORT || 3100
+var port = secrets.port;
 server.listen(port)
-console.log('Constella started on port ' + port)
+console.log('Stellar API started on port ' + port)
